29bc16f5bf2a20e4fec2211894a932d640f8d9c7
michelle912 <81959777+michelle912@users.noreply.github.com>
2021-11-17 14:41:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
 
+A = Array.isArray(a);
+B = Array.isArray(b);
+console.log(A);
+console.log(B);
+
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
+
+a = a.map(e => e*2);
+console.log(a);
 
 
 // Write the program, according to the following requirements output results.
@@ -14,13 +22,31 @@ var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
+colors_1 = colors.join(" ");
+colors_2 = colors.join("+");
+colors_3 = colors.join(",");
+
+console.log(colors_1);
+console.log(colors_2);
+console.log(colors_3);
+
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+a.sort((a, b) => b-a);
+console.log(a);
 
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+
+function mode(arr){
+    return arr.sort((a,b) =>
+          arr.filter(v => v===a).length - arr.filter(v => v===b).length
+    ).pop();
+}
+
+console.log(mode(a));
